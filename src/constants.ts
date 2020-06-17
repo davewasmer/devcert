15cabbb632b56668f28afd561f5665b15e0eb323
Michael Stramel <stramel@users.noreply.github.com>
2020-06-17 12:16:39 -0500

    Fix valid domain regex
    Based on https://gist.github.com/dperini/729294
    
    Stripped the following checks:
    
    - protocol (http, ftp)
    - short syntax (//)
    - basic auth (user:pass)
    - requirement for TLD (\. made optional)
    - port number (:port)
    - resource path (/path, #resource-path)

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,8 @@ import applicationConfigPath = require('application-config-path');
 import eol from 'eol';
 import { mktmp } from './utils';
 
-export const VALID_DOMAIN = /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/;
+// Based on https://gist.github.com/dperini/729294
+export const VALID_DOMAIN = /^(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.?)+(?:[a-z\u00a1-\uffff]{2,}\.?))$/i;
 
 // Platform shortcuts
 export const isMac = process.platform === 'darwin';

29a3ac08c415391faf3440b1d6465f67f3bc8bd1
Phil Pluckthun <phil@kitten.sh>
2025-01-22 15:33:47 +0000

    Swap sudo-prompt for @vscode/sudo-prompt

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import { execFileSync, ExecFileSyncOptions } from 'child_process';
 import tmp from 'tmp';
 import createDebug from 'debug';
 import path from 'path';
-import sudoPrompt from 'sudo-prompt';
+import sudoPrompt from '@vscode/sudo-prompt';
 
 import { configPath } from './constants';
 

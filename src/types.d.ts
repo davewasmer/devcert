798585e07e0d1808542434222cd5c451a674bba2
Phil Pluckthun <phil@kitten.sh>
2025-01-22 15:34:11 +0000

    Remove type declaration

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,5 @@
 declare module "command-exists";
 declare module "eol";
-declare module "sudo-prompt";
 declare module "password-prompt";
 declare module "application-config-path" {
   export = (appName: string) => string;
